2d3e84ad52dc7b07c9cb6f3597228384bc253fc6
Anton Evseev <eavae@mail.ru>
2021-05-27 13:15:52 +0500

    Реализовано решение Осталось реализовать проверку ввода пользователя (если пользователь ничего ни ввел либо ввел не число) Исправить вывод уравнения, посредством проверок полярности числа вывести соответствующие знаки (+ или -)

--- src/ru/evant/quadratic_equation/Main.java
+++ src/ru/evant/quadratic_equation/Main.java
@@ -9,14 +9,14 @@ import java.util.Scanner;
 
 /*
     Ход выполнения задачи:
-        1. Получить данные от пользователя значения переменных - A, B и С
+        1. Получить данные от пользователя, значения переменных - A, B и С
         2. Вычислить Дискриминант -> D = b² − 4ac
-        3. Вычислить значение переменной x
+        3. Решить уравнение
             3.1. если D < 0 -> Уравнение не имеет решений
-            3.2. если D = 0 -> Уравнение имеет один корень ->  x = -b² / 2a
+            3.2. если D = 0 -> Уравнение имеет один корень ->  x = -b / 2a
             3.3. если D > 0 -> Уравнение имеет  два кореня -> x1 = (-b - √D) / (2a)
                                                            -> x2 = (-b - √D) / (2a)
-        4. Вывести ответ
+        4. Вывести результат
 */
 
 /*
@@ -43,10 +43,16 @@ public class Main {
         /*
           Объявить и инициализировать ссылочные перменные строкового типа, тип - String.
           Которые ссылаются на строки указанные в "".
-          Необходимые для вывода результата
+          Необходимые для ввода и вывода данных
          */
-        String dZero = "Уравнение не имеет решений!";
-        String dNegativeX = "Уравнение имеет один корень.\nx = ";
+        String formula = "Решение квадратных уравнений, типа:\n    Ax² + Bx + C = 0";
+        String enterValueA = "Введите значение переменной А:\n>: ";
+        String enterValueB = "Введите значение переменной B:\n>: ";
+        String enterValueC = "Введите значение переменной C:\n>: ";
+        String equation; // уравнение
+        String dD; // дискриминант
+        String dNegative = "Уравнение не имеет корней!";
+        String dZeroX = "Уравнение имеет один корень.\nx = ";
         String dPositiveX1 = "Уравнение имеет два кореня.\nx1 = ";
         String dPositiveX2 = "\nx2 = ";
 
@@ -56,5 +62,40 @@ public class Main {
          */
         Scanner userInput = new Scanner(System.in);
 
+        System.out.println(formula);      // вывод в консоль. формула
+
+        /* 1. Получить данные от пользователя */
+        System.out.print(enterValueA);  // вывод в консоль. запрос данных
+        a = userInput.nextDouble();     // получить данные введенные пользователем и присвоить их переменной a
+        System.out.print(enterValueB);  // вывод в консоль. запрос данных
+        b = userInput.nextDouble();     // получить данные введенные пользователем и присвоить их переменной b
+        System.out.print(enterValueC);  // вывод в консоль. запрос данных
+        c = userInput.nextDouble();     // получить данные введенные пользователем и присвоить их переменной c
+
+        equation = a + "x²" + b + "x" + c;
+
+        /* 2. Вычислить Дискриминант */
+        D = Math.pow(b,2) - 4 * a * c; // Math.pow(b,2) -> использовать метод pow(x, n) класса Math -> pow - возвести число в степень, x - число, n - степень
+        dD = "D = " + D;
+
+        /* 3. Решить уравнение и 4. Вывести результат*/
+
+        if (D < 0){ /* 3.1. если (if) D < 0 */
+            System.out.println(equation);
+            System.out.println(dD);
+            System.out.println(dNegative);
+        } else if (D == 0) { /* 3.2. иначе (else) если (if) D = 0 */
+            x =((-1)*b) / (2 * a);
+            System.out.println(equation);
+            System.out.println(dD);
+            System.out.println(dZeroX + x);
+        } else { /* 3.3. иначе (else) */
+            x1 = ((-1) * b - Math.sqrt(D)) / (2 * a); // sqrt(n) -> Корень квадратный из n
+            x2 = ((-1) * b + Math.sqrt(D)) / (2 * a);
+            System.out.println(equation);
+            System.out.println(dD);
+            System.out.println(dPositiveX1 + x1);
+            System.out.println(dPositiveX2 + x2);
+        }
     }
 }
